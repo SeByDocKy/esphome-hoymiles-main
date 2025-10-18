4b0ac9d85b1d4c16d78eb9b3775831ba23195c8f
SeByDocKy <sebydocky@hotmail.com>
2025-10-18 12:42:25 +0200

    fixed persistant register according to last OpdenDTU lib

--- src/commands/ActivePowerControlCommand.h
+++ src/commands/ActivePowerControlCommand.h
@@ -6,8 +6,8 @@
 typedef enum { // ToDo: to be verified by field tests
     AbsolutNonPersistent = 0x0000, // 0
     RelativNonPersistent = 0x0001, // 1
-    AbsolutPersistent = 0x0100, // 256
-    RelativPersistent = 0x0101 // 257
+    AbsolutPersistent = 0x0002, // 2
+    RelativPersistent = 0x0003 // 3
 } PowerLimitControlType;
 
 class ActivePowerControlCommand : public DevControlCommand {
