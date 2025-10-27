bc1042627d69a2358afcc127621017ae72e9cfcf
SeByDocKy <sebydocky@hotmail.com>
2025-10-27 12:18:53 +0100

    Update Hoymiles.h

--- src/Hoymiles.h
+++ src/Hoymiles.h
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
-#ifdef HMS_INVERTER
+// #ifdef HMS_INVERTER
   #include "HoymilesRadio_CMT.h"
-#endif
-#ifdef HM_INVERTER
+// #endif
+// #ifdef HM_INVERTER
   #include "HoymilesRadio_NRF.h"
-#endif
+// #endif
 #include "inverters/InverterAbstract.h"
 #include "types.h"
 #include <Print.h>
@@ -19,12 +19,12 @@
 class HoymilesClass {
 public:
     void init();
-#ifdef HM_INVERTER
+// #ifdef HM_INVERTER
     void initNRF(SPIClass* initialisedSpiBus, const uint8_t pinCE, const uint8_t pinIRQ);
-#endif
-#ifdef HMS_INVERTER
+// #endif
+// #ifdef HMS_INVERTER
     void initCMT(const int8_t pin_sdio, const int8_t pin_clk, const int8_t pin_cs, const int8_t pin_fcs, const int8_t pin_gpio2, const int8_t pin_gpio3);
-#endif
+// #endif
     void loop();
 
     void setMessageOutput(Print* output);
@@ -36,25 +36,25 @@ public:
     std::shared_ptr<InverterAbstract> getInverterByFragment(const fragment_t& fragment);
     void removeInverterBySerial(const uint64_t serial);
     size_t getNumInverters() const;
-#ifdef HM_INVERTER
+// #ifdef HM_INVERTER
     HoymilesRadio_NRF* getRadioNrf();
-#endif
-#ifdef HMS_INVERTER
+// #endif
+// #ifdef HMS_INVERTER
     HoymilesRadio_CMT* getRadioCmt();
-#endif
+// #endif
     uint32_t PollInterval() const;
     void setPollInterval(const uint32_t interval);
 
-//    bool isAllRadioIdle() const;
+    bool isAllRadioIdle() const;
 
 private:
     std::vector<std::shared_ptr<InverterAbstract>> _inverters;
-#ifdef HM_INVERTER
+// #ifdef HM_INVERTER
     std::unique_ptr<HoymilesRadio_NRF> _radioNrf;
-#endif
-#ifdef HMS_INVERTER
+// #endif
+// #ifdef HMS_INVERTER
     std::unique_ptr<HoymilesRadio_CMT> _radioCmt;
-#endif
+// #endif
     std::mutex _mutex;
 
     uint32_t _pollInterval = 0;
