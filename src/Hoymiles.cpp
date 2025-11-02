8381dea8e22c90018450a8776ddb12a5e3640d6e
SeByDocKy <sebydocky@hotmail.com>
2025-11-02 18:55:46 +0100

    Update Hoymiles.cpp

--- src/Hoymiles.cpp
+++ src/Hoymiles.cpp
@@ -57,7 +57,7 @@ void HoymilesClass::loop()
 #ifdef HMS_INVERTER    
     _radioCmt->loop();
 #endif    
-    _messageOutput->print("# inverter "); 
+    _messageOutput->print("#inverter: "); 
     _messageOutput->println(getNumInverters());
     if (getNumInverters() == 0) {
         return;
